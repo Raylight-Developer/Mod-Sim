22361505346fb598dae86f038b2270b8e0841a03
Raylight-Developer <Raylight.creator@gmail.com>
2024-08-29 14:09:32 -0600

    Plots

--- Proyecto-1/src/Params.hpp
+++ Proyecto-1/src/Params.hpp
@@ -4,7 +4,7 @@
 
 #define SAMPLES 10
 #define SHIFT 1e-8
-#define SIM_TIME 50.0
+#define SIM_TIME 5.0
 #define BOUNDING_BOX QRectF(-200, 0, 400, 800)
 
 #define TIME_SCALE 5.0
--- Proyecto-1/src/main.cpp
+++ Proyecto-1/src/main.cpp
@@ -5,9 +5,17 @@
 
 #include "Params.hpp";
 
+#undef slots
+
+#define WITHOUT_NUMPY
+#include "matplotlibcpp.h"
+namespace plt = matplotlibcpp;
+
 struct ParticleSimulation : QGraphicsScene {
+	map<uint64, map<Particle*, tuple<vector<uint64>, vector<dvec2>, vector<dvec2>, vector<dvec1>, vector<dvec2>>>> system_data;
 	vector<vector<Particle*>> systems;
 	QRectF bounding_box;
+	QElapsedTimer timer;
 
 	ParticleSimulation(QMainWindow* parent = nullptr) :
 		QGraphicsScene(parent)
@@ -23,10 +31,13 @@ struct ParticleSimulation : QGraphicsScene {
 
 	void setup_particles() {
 		for (uint64 i = 0; i < SAMPLES; ++i) {
+			system_data[i] = {};
 			vector<Particle*> params;
-			for (const auto& param : PARAMETERS) {
+			for (auto param : PARAMETERS) {
+				param.system_id = to_string(i);
 				auto particle = new Particle(param);
 				params.push_back(particle);
+				system_data[i][particle] = tuple<vector<uint64>, vector<dvec2>, vector<dvec2>, vector<dvec1>, vector<dvec2>>({}, {}, {}, {}, {});
 			}
 			params[3]->setCenter(params[3]->params.center + dvec2(SHIFT * ul_to_f(i), 0));
 			systems.push_back(params);
@@ -34,7 +45,7 @@ struct ParticleSimulation : QGraphicsScene {
 
 		for (uint64 i = 0; i < systems.size(); ++i) {
 			QColor color;
-			color.setHsv(d_to_i((i / ul_to_d(SAMPLES)) * 360.0), 150, 255);
+			color.setHsv(d_to_i((i / ul_to_d(SAMPLES)) * 360.0), 150, 255, 100);
 
 			for (uint64 j = 0; j < systems[i].size(); ++j) {
 				const auto& particle = systems[i][j];
@@ -56,6 +67,11 @@ struct ParticleSimulation : QGraphicsScene {
 					auto& particle_b = systems[i][k];
 					particle_a->handle_particle_collision(particle_b);
 				}
+				get<0>(system_data[i][particle_a]).push_back(timer.elapsed());
+				get<1>(system_data[i][particle_a]).push_back(particle_a->params.center);
+				get<2>(system_data[i][particle_a]).push_back(particle_a->params.velocity);
+				get<3>(system_data[i][particle_a]).push_back(particle_a->params.angular_velocity);
+				get<4>(system_data[i][particle_a]).push_back(particle_a->params.acceleration);
 			}
 		}
 	}
@@ -82,6 +98,7 @@ struct MainWindow : QMainWindow {
 	}
 
 	void init() {
+		simulation->timer.start();
 		timer = new QTimer(this);
 		connect(timer, &QTimer::timeout, this, &MainWindow::update_scene);
 		timer->start(0);
@@ -116,6 +133,72 @@ struct MainWindow : QMainWindow {
 	void finish() {
 		timer->stop();
 		fps_timer->stop();
+
+		for (const auto& [system_id, system] : simulation->system_data) {
+			if (system_id == 0 || system_id == 1) {
+				for (const auto& [particle, data] : system) {
+					vector<double> x_vals, y_vals;
+
+					const auto& [timestamps_uint64, positions, velocities, angular_velocities, accelerations] = data;
+					vector<double> timestamps(timestamps_uint64.size());
+					transform(timestamps_uint64.begin(), timestamps_uint64.end(), timestamps.begin(), [](uint64_t value) { return static_cast<double>(value) / 1000.0; });
+
+					auto extract_component = [](const vector<dvec2>& vec, int component) {
+						vector<double> result;
+						for (const auto& v : vec) {
+							result.push_back(component == 0 ? v.x : v.y);
+						}
+						return result;
+						};
+
+					// Plot Positions
+					auto pos_x = extract_component(positions, 0);
+					auto pos_y = extract_component(positions, 1);
+					plt::plot(timestamps, pos_x, {{"label", "x"}});
+					plt::plot(timestamps, pos_y, {{"label", "y"}});
+					plt::xlabel("Time");
+					plt::ylabel("Position");
+					plt::legend();
+					plt::title("Positions");
+					plt::save("./Outputs/Position_" + to_string(system_id) + "__" + particle->params.name + ".png");
+					plt::clf();
+
+					// Plot Velocities
+					auto vel_x = extract_component(velocities, 0);
+					auto vel_y = extract_component(velocities, 1);
+					plt::plot(timestamps, vel_x, {{"label", "x"}});
+					plt::plot(timestamps, vel_y, {{"label", "y"}});
+					plt::xlabel("Time");
+					plt::ylabel("Velocity");
+					plt::legend();
+					plt::title("Velocities");
+					plt::save("./Outputs/Velocity_" + to_string(system_id) + "__" + particle->params.name + ".png");
+					plt::clf();
+
+					// Plot Angular Velocities
+					auto vel = angular_velocities;
+					plt::plot(timestamps, vel_x);
+					plt::xlabel("Time");
+					plt::ylabel("Angular Velocity");
+					plt::legend();
+					plt::title("Angular Velocity");
+					plt::save("./Outputs/Angular_Velocity_" + to_string(system_id) + "__" + particle->params.name + ".png");
+					plt::clf();
+
+					// Plot Accelerations
+					auto acc_x = extract_component(accelerations, 0);
+					auto acc_y = extract_component(accelerations, 1);
+					plt::plot(timestamps, acc_x, {{"label", "x"}});
+					plt::plot(timestamps, acc_y, {{"label", "y"}});
+					plt::xlabel("Time");
+					plt::ylabel("Acceleration");
+					plt::legend();
+					plt::title("Accelerations");
+					plt::save("./Outputs/Acceleration_" + to_string(system_id) + "__" + particle->params.name + ".png");
+					plt::clf();
+				}
+			}
+		}
 		QApplication::quit();
 	}
 
